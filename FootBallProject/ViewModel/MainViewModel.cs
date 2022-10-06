460d193ee93cdc639c01f7f60fe3eb32f7bb5410
tuonghuynh11 <21520123@ms.uit.edu.vn>
2022-10-06 08:47:58 +0700

    Add project files.

---
+++ FootBallProject/ViewModel/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootBallProject.ViewModel
+{
+    public class MainViewModel :BaseViewModel
+    {
+        ///Mọi xử lý nằm trong này
+    }
+}
