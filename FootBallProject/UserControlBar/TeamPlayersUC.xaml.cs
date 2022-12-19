c175e033d9f58735c71757b0f60a9c72d8839e90
QuocBaoKho <21520613@gm.uit.edu.vn>
2022-12-19 22:03:03 +0700

    Them user control, tao anh default player

--- FootBallProject/UserControlBar/TeamPlayersUC.xaml.cs
+++ FootBallProject/UserControlBar/TeamPlayersUC.xaml.cs
@@ -10,18 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace FootBallProject
+namespace FootBallProject.UserControlBar
 {
     /// <summary>
-    /// Interaction logic for TransferWindow.xaml
+    /// Interaction logic for TeamPlayersUC.xaml
     /// </summary>
-
-    public partial class TransferWindow : UserControl
-
+    public partial class TeamPlayersUC : UserControl
     {
-        public TransferWindow()
+        public TeamPlayersUC()
         {
             InitializeComponent();
         }
